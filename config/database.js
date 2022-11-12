f77ed021c049182d0a725f2d93c3e404435a967e
Burhan Ali <burhanali.dev@gmail.com>
2022-11-11 22:19:29 -0500

    intial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://burhanali:2short2me@cluster0.e7moc1a.mongodb.net/college-application-clipboard?retryWrites=true&w=majority', 
+    'dbName': 'college-application-clipboard'
+};
